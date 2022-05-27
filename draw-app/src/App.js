e7cfa193f061dd9d4feca2cdafda49119474c4ca
hericlesferraz <hericles@sensix.ag>
2022-05-27 18:25:51 -0300

    feat: Components to draw

---
+++ draw-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Draw from './components/Draw';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Classificador de Digitos </h1>
+      <Draw />
+    </div>
+  );
+}
+
+export default App;
